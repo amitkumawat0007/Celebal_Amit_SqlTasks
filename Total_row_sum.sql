db4cef589e2afaf7c3531b65cf333e1b5f40d353
Amit Kumawat <amitkumawat0007@gmail.com>
2018-05-03 14:51:47 +0530

    Revert "again change in file"
    This reverts commit 99f00f37596760282c22d7ef08ecc7253c813a9c.

--- Total_row_sum.sql
+++ Total_row_sum.sql
@@ -2,6 +2,7 @@ select * from [dbo].[Revenue Target];
 select * from [dbo].[in$];
 select * from [dbo].[List of GMC];
 --to find sum of whole row for individual id and F2 id anaplan id.
+--testing to sync changes in file with github.
 SELECT
     F2 as ID
 
